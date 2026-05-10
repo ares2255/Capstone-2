33fbbd3abbc074d375782d1302d15493105638cc
ares2255 <aria.hurtada.ui@phinmaed.com>
2026-05-10 17:19:43 +0800

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -2,6 +2,7 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 include "config/db.php";
+date_default_timezone_set('Asia/Manila');
 header('Content-Type: application/json');
 
 $input = trim($_POST['username'] ?? '');
