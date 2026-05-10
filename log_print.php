e6186d12822c54cbe9c03527d71f87ce05e9f2c2
ares2255 <aria.hurtada.ui@phinmaed.com>
2026-05-10 17:18:32 +0800

    Update log_print.php

--- log_print.php
+++ log_print.php
@@ -1,7 +1,8 @@
 <?php
 include "config/db.php";
+date_default_timezone_set('Asia/Manila');
 
-$stmt = $pdo->prepare("INSERT INTO print_jobs (type, pages, price, created_at) VALUES (:t, :p, :pr, NOW())");
-$stmt->execute([':t' => $_POST['type'], ':p' => $_POST['pages'], ':pr' => $_POST['price']]);
+$stmt = $pdo->prepare("INSERT INTO print_jobs (type, pages, price, created_at) VALUES (:t, :p, :pr, :ts)");
+$stmt->execute([':t' => $_POST['type'], ':p' => $_POST['pages'], ':pr' => $_POST['price'], ':ts' => date('Y-m-d H:i:s')]);
 echo "success";
 ?>
