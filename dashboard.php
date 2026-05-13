860b1ab9e36f1b5104aae89a9b45cbad7f343848
ares2255 <aria.hurtada.ui@phinmaed.com>
2026-05-13 16:02:54 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -59,6 +59,7 @@ try {
 <head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1">
 <title>The Desktop | Dashboard</title>
+<link rel="icon" type="image/jpeg" href="q.jpg">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="includes/navbar.css">
